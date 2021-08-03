520ff8b3200362ffaf5a8b1b5a7d120df0168e9c
CarlaCorreia93 <86625791+CarlaCorreia93@users.noreply.github.com>
2021-08-02 22:50:41 -0300

    Adicionei arquivos via upload

---
+++ Hello Word!/src/Primeiro_projeto/HelloWord.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package Primeiro_projeto;
+
+/**
+ * @author Carla
+ * Primeiro projeto Java
+ */
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+
+	}
+
+}
